856333867374084b15c910fc56011f03c8fe6e73
Maria Blair <maria.klachko@gmail.com>
2016-09-21 18:34:51 -0700

    Added some testing code to battleship-v2.js

--- Version 2 - Object-oriented design /battleship-v2.js
+++ Version 2 - Object-oriented design /battleship-v2.js
@@ -100,6 +100,21 @@ console.log(model.shipsSunk);
 model.fire('51'); // 3rd hit
 console.log(model.shipsSunk); // should be 1 now
 
+// some more testing
+// model.fire("53"); // miss
+
+// model.fire("31"); // hit
+// model.fire("41"); // hit
+// model.fire("51"); // hit
+
+// model.fire("34"); // hit
+// model.fire("24"); // hit
+// model.fire("14"); // hit
+
+// model.fire("00"); // hit
+// model.fire("01"); // hit
+// model.fire("02"); // hit
+
 
 
 
